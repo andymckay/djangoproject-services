0085ea0ade03275da8fd50dd39a1ab98fee631d2
John Boxall <john@handimobility.ca>
2010-03-03 11:34:57 -0800

    lalala

--- services/views.py
+++ services/views.py
@@ -7,16 +7,16 @@ def company_list(request):
     queryset = Company.objects.all()
     kwargs = {
         "template_object_name": "company",
-        "template_name": "company/company_list.html",
+        "template_name": "services/company_list.html",
         "extra_context": {},        
     }
     return object_list(request, queryset, **kwargs)
 
-def company_detail(request, slug=None, slug_field="slug"):
-    pass
-#     kwargs = {
-#         "queryset",
-#         "slug_field": slug_field,
-#     
-#     }
-#     return object_detail(**kwargs)
+def company_detail(request, slug):
+    queryset = Company.objects.all()
+    kwargs = {
+        "template_object_name": "company",
+        "template_name": "services/company_detail.html",
+        "extra_context": {},
+    }
+    return object_detail(request, queryset, **kwargs)
