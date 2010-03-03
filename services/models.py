e60dd423dff8e157c0fdf16f3edb9fb950f031d1
Andy McKay <andy@a159.blockc-86.stargate.ca>
2010-03-03 10:52:59 -0800

    services

---
+++ services/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+states = (
+    (1, "unapproved"),
+    (2, "approved"),
+    (3, "denied"),
+)
+
+levels = (
+    (1, "none"),
+    (2, "sponsor"),
+)
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    logo = models.ImageField(upload_to="companies")
+    country = models.CharField(max_length=255)
+    state = models.IntegerField(choices=states)
+    sponsorship_level = models.IntegerField(choices=levels)
+    created_at = models.DateTimeField(auto_now_add=True)
+    url = models.URLField()
+    slug = models.SlugField()
+    
+    def __unicode__(self):
+        return self.name
